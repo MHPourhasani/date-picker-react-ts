8549d9395a0f71634d4d4e5a1d7d8e715fd084da
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-03-05 02:42:41 +0330

    configuring and customizing Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			iranyekan: ['iranyekan'],
+		},
+		fontSize: {
+			11: '11px',
+			14: '14px',
+			15: '15px',
+			16: '16px',
+		},
+		colors: {
+			white: '#fff',
+			primary: '#3880FF',
+			secondary300: '#D9DBE9',
+			secondary400: '#A0A3BD',
+			secondary600: '#4E4B66',
+			secondary800: '#14142A',
+		},
+		borderWidth: { 1: '1px', 1.5: '1.5px' },
+		boxShadow: {
+			calendar: '-4px 4px 16px rgba(0, 0, 0, 0.08)',
+		},
 	},
+	extend: {},
 	plugins: [],
 };
