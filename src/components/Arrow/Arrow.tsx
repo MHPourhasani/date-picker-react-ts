d62c9080f4b69f0f843bc85a07a3c9195f2b421c
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-03-06 02:53:19 +0330

    change Arrow component to typeScript

--- src/components/Arrow/Arrow.tsx
+++ src/components/Arrow/Arrow.tsx
@@ -1,13 +1,14 @@
 import { ReactComponent as ArrowRight } from '../../assets/svg/arrow-right.svg';
+import { ArrowProps } from '../../types/Arrow';
 
-const Arrow = ({ direction, onClick, disabled }) => {
+const Arrow: React.FC<ArrowProps> = ({ direction, onClick, disabled }) => {
 	return (
 		<span
 			className={`flex cursor-pointer items-center justify-center rounded-full hover:text-primary ${direction} ${
 				disabled ? 'text-secondary400' : ''
 			}`}
 			onClick={onClick}>
-			{direction === 'right' ? <ArrowRight className='rotate-180' /> : <ArrowRight />}
+			{direction === 'right' ? <ArrowRight /> : <ArrowRight className='rotate-180' />}
 		</span>
 	);
 };
